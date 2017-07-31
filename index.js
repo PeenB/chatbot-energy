3484c5b3467437a1f17e9f6bf5a43695efcf9458
Buranachon <macpro@Macpros-MacBook-Pro.local>
2017-07-31 13:24:18 +0700

    modified

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ let token = "EAAaEFbZBT8Q4BAIKPCaJD3JxZBSdvoZA3GkULf3SoMfttfCjoZBZAOq5IHYOaZC2Jk
 
  app.post('/webhook/', function(req, res){
    let messaging_events = req.body.entry[0].messaging_events
+   console.log('messaging_events', messaging_events)
    for (let i =0; i<messaging_events.length; i++){
      let event = messaging_events[i]
      let sender = event.sender.id
