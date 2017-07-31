b76dcc446a54a8539ca4bf431d533abb00b408e1
Buranachon <macpro@Macpros-MacBook-Pro.local>
2017-07-31 13:47:55 +0700

    modified

--- index.js
+++ index.js
@@ -28,38 +28,68 @@ let token = "EAAaEFbZBT8Q4BAIKPCaJD3JxZBSdvoZA3GkULf3SoMfttfCjoZBZAOq5IHYOaZC2Jk
    res.send("Wrong Token!!!")
  })
 
- app.post('/webhook/', function(req, res){
-   let messaging_events = req.body.entry[0].messaging_events
-   console.log('messaging_events', messaging_events)
-   for (let i =0; i<messaging_events.length; i++){
-     let event = messaging_events[i]
-     let sender = event.sender.id
-     if(event.message && event.message.text){
-       let text = event.message.text
-       sendText(sender, "Text echo:" + text.substring(0,100))
-     }
+ app.post("/webhook", function (req, res) {
+   // Make sure this is a page subscription
+   if (req.body.object == "page") {
+     // Iterate over each entry
+     // There may be multiple entries if batched
+     req.body.entry.forEach(function(entry) {
+       // Iterate over each messaging event
+       entry.messaging.forEach(function(event) {
+         if (event.postback) {
+           processPostback(event);
+         }
+       });
+     });
+
+     res.sendStatus(200);
    }
-   res.sendStatus(200)
- })
+ });
 
+ function processPostback(event) {
+   var senderId = event.sender.id;
+   var payload = event.postback.payload;
 
- function sendText(sender, text){
-   let messageData = {text: text}
+   if (payload === "Greeting") {
+     // Get user's first name from the User Profile API
+     // and include it in the greeting
+     request({
+       url: "https://graph.facebook.com/v2.6/" + senderId,
+       qs: {
+         access_token: process.env.PAGE_ACCESS_TOKEN,
+         fields: "first_name"
+       },
+       method: "GET"
+     }, function(error, response, body) {
+       var greeting = "";
+       if (error) {
+         console.log("Error getting user's name: " +  error);
+       } else {
+         var bodyObj = JSON.parse(body);
+         name = bodyObj.first_name;
+         greeting = "Hi " + name + ". ";
+       }
+       var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+       sendMessage(senderId, {text: message});
+     });
+   }
+ }
+
+ // sends message to user
+ function sendMessage(recipientId, message) {
    request({
-     url: "https://graph.facebook.com/v2.10/me/messages",
-     qs : {access_token, token},
+     url: "https://graph.facebook.com/v2.6/me/messages",
+     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
      method: "POST",
      json: {
-       receipt: {id: sender},
-       message: messageData
+       recipient: {id: recipientId},
+       message: message,
      }
-   }, function (error,response,body){
-     if (error){
-       console.log("sending error")
-     } else if (response.body.error) {
-       console.log("response body error")
+   }, function(error, response, body) {
+     if (error) {
+       console.log("Error sending message: " + response.error);
      }
-   })
+   });
  }
 
 app.listen(app.get('port'), function(){
