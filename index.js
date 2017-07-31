23baa589eb537bd7b3b41d5353630275ba21b6e8
Buranachon <macpro@Macpros-MacBook-Pro.local>
2017-07-31 13:50:20 +0700

    modified

--- index.js
+++ index.js
@@ -1,97 +1,25 @@
-'use strict'
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-const app = express()
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
 
-app.set('port', (process.env.PORT || 5000))
-
-//Process the data
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-//Route
-
-app.get('/', function(req, res){
-  res.send("Hi, I'm a chatbot")
-})
-
-let token = "EAAaEFbZBT8Q4BAIKPCaJD3JxZBSdvoZA3GkULf3SoMfttfCjoZBZAOq5IHYOaZC2Jkz4EQtvBh2SZCUz4gnPxiBndPKZCBaQwJutwbWrXURlOnWvDtpk5r4qfHdfwdNqSOj8PJC7dXZC7XAXqCVqYezrXvFjKYnOlJXuyBP7cufZBQUAZDZD"
- //Facebook
-
- app.get('/webhook/', function(req, res){
-   if(req.query['hub.verify_token'] === "blondiebytes"){
-     res.send(req.query['hub.challenge'])
-   }
-   res.send("Wrong Token!!!")
- })
-
- app.post("/webhook", function (req, res) {
-   // Make sure this is a page subscription
-   if (req.body.object == "page") {
-     // Iterate over each entry
-     // There may be multiple entries if batched
-     req.body.entry.forEach(function(entry) {
-       // Iterate over each messaging event
-       entry.messaging.forEach(function(event) {
-         if (event.postback) {
-           processPostback(event);
-         }
-       });
-     });
-
-     res.sendStatus(200);
-   }
- });
-
- function processPostback(event) {
-   var senderId = event.sender.id;
-   var payload = event.postback.payload;
-
-   if (payload === "Greeting") {
-     // Get user's first name from the User Profile API
-     // and include it in the greeting
-     request({
-       url: "https://graph.facebook.com/v2.6/" + senderId,
-       qs: {
-         access_token: process.env.PAGE_ACCESS_TOKEN,
-         fields: "first_name"
-       },
-       method: "GET"
-     }, function(error, response, body) {
-       var greeting = "";
-       if (error) {
-         console.log("Error getting user's name: " +  error);
-       } else {
-         var bodyObj = JSON.parse(body);
-         name = bodyObj.first_name;
-         greeting = "Hi " + name + ". ";
-       }
-       var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-       sendMessage(senderId, {text: message});
-     });
-   }
- }
-
- // sends message to user
- function sendMessage(recipientId, message) {
-   request({
-     url: "https://graph.facebook.com/v2.6/me/messages",
-     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-     method: "POST",
-     json: {
-       recipient: {id: recipientId},
-       message: message,
-     }
-   }, function(error, response, body) {
-     if (error) {
-       console.log("Error sending message: " + response.error);
-     }
-   });
- }
-
-app.listen(app.get('port'), function(){
-  console.log("running: port")
-})
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
