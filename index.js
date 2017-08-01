5184c506863376113ef982b5fdd7d98c8c8ec28a
Buranachon <macpro@Macpros-MacBook-Pro.local>
2017-08-01 11:12:23 +0700

    lol

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.post('/webhook', (req, res) => {
   if (req.body.object === 'page') {
     req.body.entry.forEach((entry) => {
       entry.messaging.forEach((event) => {
-        if ((now.setTimezone("Asia/Bangkok").toLocaleTimeString().substring(0,3)) === '11' ) {
-          if((now.setTimezone("Asia/Bangkok").toLocaleTimeString().substring(3,5)) === '00'){
+        if ((now.setTimezone("Asia/Bangkok").toLocaleTimeString().substring(0,1)) === '11' ) {
+          if((now.setTimezone("Asia/Bangkok").toLocaleTimeString().substring(3,4)) === '15'){
             sendMessage(event);
           }
         }
