04957f80853d739c9a2f01db52b41b84c0dc07f5
NOK <nokkanato@gmail.com>
2017-08-03 09:23:27 +0700

    successfully add time

--- index.js
+++ index.js
@@ -1,10 +1,96 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const schedule = require('node-schedule');
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+///time alert///
+
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    |
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+
+// this is every 10 second of a minute
+var j = schedule.scheduleJob('10 * * * * *', function(){
+  sendTextMessage('100020773937674' , 'close aircondition')
+  console.log('The answer to life, the universe, and everything!');
+});
+
+///////////
+
+
+
+
+
+
+////
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+      }
+    };
+    callSendAPI(messageData);
+
+  }
+
+  function callSendAPI(messageData) {
+
+
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: "EAAaEFbZBT8Q4BAEwstNB1gjujiEiOgYbMSHbWZAG6y3b7j3ZC7IbeFevFhl59I3o3GvZBr5wdsxWP9zMVSpjC5mIYROrPNFqPZByZAZA2JyEcERzbFUJPuo9omSsGStObJNA1RkXbsZAMVeaOQWoa8DtRQfrdWZAHnygYpWf2buTdgwZDZD" },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+
+
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+////
+
+
+
+
+
+
+
+
+
+
+
 //Time
 var time = require('time');
 // Create a new Date instance, representing the current instant in time
